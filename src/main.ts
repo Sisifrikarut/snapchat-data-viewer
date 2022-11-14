566191f6f8326581be3db34c48cb3faef1319ac9
x280 <0x280@protonmail.com>
2022-11-14 08:27:20 +0100

    fix vuetify icons, restructure vuetify plugin integration

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,6 @@ import router from "./router";
 import store from "./store";
 
 // Vuetify
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+import vuetify from "./plugins/vuetify";
 
 createApp(App).use(store).use(router).use(vuetify).mount("#app");

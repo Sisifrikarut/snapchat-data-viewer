a1491cb0e6667c4ed97c446add77b6b3be932e53
x280 <86424319+0x280@users.noreply.github.com>
2022-11-15 11:29:43 +0100

    login history view

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,20 +1,62 @@
 <template>
   <div class="account">
-    {{ JSON.stringify(state.account) }}
+    <v-container v-if="uploaded">
+      <v-row justify="space-around">
+        <v-card min-width="50vw">
+          <v-card-title>Login-History</v-card-title>
+          <v-card-text>
+            <v-timeline align="center" density="comfortable">
+              <v-timeline-item
+                v-for="(item, i) in account.loginHistory"
+                :key="i"
+                size="small"
+              >
+                <v-card flat>
+                  <v-card-title class="text-primary">
+                    {{ item.ip }}
+                  </v-card-title>
+                  <v-card-subtitle>
+                    {{ item.createdAt.toLocaleString() }}
+                  </v-card-subtitle>
+                  <v-card-text>
+                    <v-chip class="ma-2">{{ item.country }}</v-chip>
+                    <v-chip
+                      class="ma-2"
+                      :color="item.status == 'success' ? 'success' : 'error'"
+                      >{{ item.status }}</v-chip
+                    >
+                  </v-card-text>
+                </v-card>
+              </v-timeline-item>
+            </v-timeline>
+          </v-card-text>
+        </v-card>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { Account } from "@/types/account.interface";
 
 @Options({
+  data: () => ({
+    colors: ["cyan", "green", "pink", "amber", "orange"],
+  }),
   computed: {
     state() {
       return this.$store.state;
     },
+    account() {
+      return this.state.account as Account;
+    },
+    uploaded() {
+      return this.state.uploaded;
+    },
   },
   created() {
-    if (!this.state.uploaded) {
+    if (!this.uploaded) {
       this.$router.push("/");
     }
   },
@@ -23,7 +65,7 @@ export default class HomeView extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-.home {
+.account {
   margin-inline: 1rem;
   text-align: center;
   height: 100%;

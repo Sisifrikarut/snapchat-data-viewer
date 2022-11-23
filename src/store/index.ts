41a8ec0244c2e4df2deea381c9b56cb12ec8529d
x280 <86424319+0x280@users.noreply.github.com>
2022-11-23 20:38:09 +0100

    add AccountHistory structures and parsing logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,21 @@
 import { createStore } from "vuex";
-import { Account, ExportAccount } from "@/types/account.interface";
+import {
+  Account,
+  AccountHistory,
+  ExportAccount,
+  ExportAccountHistory,
+} from "@/types/account.interface";
 import * as zip from "jszip";
 
 export default createStore({
   state: {
     uploaded: false,
     account: undefined as Account | undefined,
+    accountHistory: undefined as AccountHistory | undefined,
   },
   getters: {
     account: (state) => state.account,
+    accountHistory: (state) => state.accountHistory,
   },
   mutations: {
     parseZipData(state, buffer: ArrayBuffer | null) {
@@ -16,54 +23,20 @@ export default createStore({
         zip.loadAsync(buffer).then((zip) => {
           zip.forEach((relativePath, zipEntry) => {
             if (relativePath.endsWith("account.json")) {
-              // eslint-disable-next-line
-              // @ts-ignore
-              this.commit("parseAccount", zipEntry);
+              zipEntry.async("string").then((data) => {
+                const account = JSON.parse(data) as ExportAccount;
+                state.account = new Account(account);
+              });
+            } else if (relativePath.endsWith("account_history.json")) {
+              zipEntry.async("string").then((data) => {
+                const accountHistory = JSON.parse(data) as ExportAccountHistory;
+                state.accountHistory = new AccountHistory(accountHistory);
+              });
             }
           });
         });
         state.uploaded = true;
       }
-    },
-    parseAccount(state, zipEntry: zip.JSZipObject) {
-      zipEntry.async("string").then((data) => {
-        const account = JSON.parse(data) as ExportAccount;
-        const parseDate = (date: string) => new Date(date.replace(" UTC", "Z"));
-        state.account = {
-          username: account["Basic Information"].Username,
-          name: account["Basic Information"].Name,
-          creationDate: parseDate(
-            account["Basic Information"]["Creation Date"]
-          ),
-          currentDevice: {
-            make: account["Device Information"].Make,
-            modelId: account["Device Information"]["Model ID"],
-            modelName: account["Device Information"]["Model Name"],
-            userAgent: account["Device Information"]["User Agent"],
-            language: account["Device Information"].Language,
-            osType: account["Device Information"]["OS Type"],
-            osVersion: account["Device Information"]["OS Version"],
-            connectionTypes: account["Device Information"]["Connection Type"]
-              .split(", ")
-              .map((type) => type.trim()),
-          },
-          deviceHistory: account["Device History"].map((device) => ({
-            make: device.Make,
-            model: device.Model,
-            startTime: parseDate(device["Start Time"]),
-            deviceType: device["Device Type"],
-          })),
-          loginHistory: account["Login History"].map((login) => ({
-            ip: login.IP,
-            country: login.Country,
-            createdAt: parseDate(login.Created),
-            status: login.Status,
-          })),
-        };
-        state.account.deviceHistory.sort(
-          (a, b) => b.startTime.getTime() - a.startTime.getTime()
-        );
-      });
     },
   },
   actions: {},
--- src/types/account.interface.ts
+++ src/types/account.interface.ts
@@ -32,7 +32,7 @@ export interface ExportAccount {
   "Family Center": [];
 }
 
-export interface Account {
+export class Account {
   username: string;
   name: string;
   creationDate: Date;
@@ -58,4 +58,138 @@ export interface Account {
     createdAt: Date;
     status: string;
   }[];
+
+  constructor(account: ExportAccount) {
+    const parseDate = (date: string) => new Date(date.replace(" UTC", "Z"));
+
+    this.username = account["Basic Information"].Username;
+    this.name = account["Basic Information"].Name;
+    this.creationDate = parseDate(
+      account["Basic Information"]["Creation Date"]
+    );
+    this.currentDevice = {
+      make: account["Device Information"].Make,
+      modelId: account["Device Information"]["Model ID"],
+      modelName: account["Device Information"]["Model Name"],
+      userAgent: account["Device Information"]["User Agent"],
+      language: account["Device Information"].Language,
+      osType: account["Device Information"]["OS Type"],
+      osVersion: account["Device Information"]["OS Version"],
+      connectionTypes: account["Device Information"]["Connection Type"]
+        .split(", ")
+        .map((type) => type.trim()),
+    };
+    this.deviceHistory = account["Device History"]
+      .map((device) => ({
+        make: device.Make,
+        model: device.Model,
+        startTime: parseDate(device["Start Time"]),
+        deviceType: device["Device Type"],
+      }))
+      .sort((a, b) => b.startTime.getTime() - a.startTime.getTime());
+    this.loginHistory = account["Login History"]
+      .map((login) => ({
+        ip: login.IP,
+        country: login.Country,
+        createdAt: parseDate(login.Created),
+        status: login.Status,
+      }))
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
+  }
+}
+
+export interface ExportAccountHistory {
+  "Display Name Change": {
+    Date: string;
+    "Display Name": string;
+  }[];
+  "Email Change": {
+    Date: string;
+    "Email Address": string;
+  }[];
+  "Mobile Number Change": {
+    Date: string;
+    "Mobile Number": string;
+  }[];
+  "Password Change": {
+    Date: string;
+  }[];
+  "Snapchat Linked to Bitmoji": {
+    Date: string;
+  }[];
+  Spectacles: [];
+  "Tow-Factor Authentication": [];
+  "Account deactivated / reactivated": [];
+  "Download My Data Reports": {
+    Date: string;
+    Status: string;
+    "Email Address": string;
+  }[];
+}
+
+export class AccountHistory {
+  nameChanges: {
+    date: Date;
+    name: string;
+  }[];
+  emailChanges: {
+    date: Date;
+    email: string;
+  }[];
+  mobileNumberChanges: {
+    date: Date;
+    mobileNumber: string;
+  }[];
+  passwordChanges: {
+    date: Date;
+  }[];
+  snapchatLinkedToBitmoji: { date: Date }[];
+  downloadMyDataReports: {
+    date: Date;
+    status: string;
+    email: string;
+  }[];
+
+  constructor(accountHistory: ExportAccountHistory) {
+    this.nameChanges = accountHistory["Display Name Change"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+        name: change["Display Name"],
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+    this.emailChanges = accountHistory["Email Change"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+        email: change["Email Address"],
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+    this.mobileNumberChanges = accountHistory["Mobile Number Change"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+        mobileNumber: change["Mobile Number"],
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+    this.passwordChanges = accountHistory["Password Change"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+    this.snapchatLinkedToBitmoji = accountHistory["Snapchat Linked to Bitmoji"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+    this.downloadMyDataReports = accountHistory["Download My Data Reports"]
+      .map((change) => ({
+        date: new Date(change.Date.replace(" UTC", "Z")),
+        status: change.Status,
+        email: change["Email Address"],
+      }))
+      .sort((a, b) => b.date.getTime() - a.date.getTime());
+  }
 }
